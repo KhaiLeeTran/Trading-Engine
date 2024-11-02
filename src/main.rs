bda14e8773c9d327f66007484b8f7ec45f5a8649
KhaiLeeTran <khailtq.2004@gmail.com>
2024-11-03 01:13:33 +0700

    add orderbook

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
 enum BidOrAsk {
     Bid,
     Ask,
 }
 
 #[derive(Debug)]
+struct  Orderbook {
+    asks: HashMap<Price, Limit>,
+    bids: HashMap<Price, Limit>,
+}
+
+
+impl Orderbook {
+    fn new() -> Orderbook{
+        Orderbook{
+            asks: HashMap::new(),
+            bids: HashMap::new(),
+        }
+    }
+
+    fn add_order(&mut self,price: f64, order: Order) {
+        match  order.bid_or_ask {
+            BidOrAsk::Bid => {
+                let price = Price::new(price);
+
+                match self.bids.get_mut(&price) {
+                    Some(limit) => limit.add_order(order),
+                    None => {
+                        let mut limit = Limit::new(price);
+                        limit.add_order(order);
+                        self.bids.insert(price, limit);
+                    }
+                }
+            }
+
+            BidOrAsk::Ask => {
+
+            }
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
 struct Price{
     intergral: u64,
     fractional: u64,
@@ -22,11 +62,28 @@ impl Price {
         }
     }
 }
+
+#[derive(Debug)]
 struct Limit {
     price: Price,
     orders: Vec<Order>,
 }
 
+
+impl Limit {
+    fn new(price: Price) -> Limit{
+        Limit{
+            price,
+            orders: Vec::new(),
+        }
+    }
+
+    fn add_order(&mut self, order: Order) {
+        self.orders.push(order);
+    }
+}
+
+#[derive(Debug)]
 struct Order{
     size: f64,
     bid_or_ask: BidOrAsk,
@@ -38,6 +95,11 @@ impl  Order {
     }
 }
 fn main() {
-    let price = Price::new(50.5);
-    println!("{:?}", price);
+    let buy_order_from_alice = Order::new(BidOrAsk::Bid, 5.5);
+    // let sell_order = Order::new(BidOrAsk::Ask, 2.45);
+    let buy_order_from_bob = Order::new(BidOrAsk::Bid, 2.45);
+    let mut  orderbook = Orderbook::new();
+    orderbook.add_order(4.4, buy_order_from_alice);
+    orderbook.add_order(4.4, buy_order_from_bob);
+    println!("{:?}", orderbook);
 }
